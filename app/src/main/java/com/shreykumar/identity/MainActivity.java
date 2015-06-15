cb6c57ece96e588a372552c26000e832fdc35645
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-06-14 23:36:58 -0400

    added basic stuff

---
+++ app/src/main/java/com/shreykumar/identity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.shreykumar.identity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+    EditText nameTxt, phoneTxt, emailTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTxt = (EditText) findViewById(R.id.name);
+        phoneTxt = (EditText) findViewById(R.id.number);
+        emailTxt = (EditText) findViewById(R.id.email);
+
+        Button submit = (Button) findViewById(R.id.add_btn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
